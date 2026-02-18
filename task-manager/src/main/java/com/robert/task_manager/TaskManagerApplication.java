0cb7d5cd9f973e1c422571388e941a1bbaee7d02
Robert <mitricarobert10@gmail.com>
2026-02-18 15:51:22 +0200

    Initial commit - Task Manager functional

---
+++ task-manager/src/main/java/com/robert/task_manager/TaskManagerApplication.java
@@ -0,0 +1,18 @@
+package com.robert.task_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagerApplication.class, args);
+	}
+
+
+	//TODO Adăugarea stratului de Service (TaskService.java)
+	//TODO metoda de DELETE,PUT, PATCH în TaskController
+
+}
+
