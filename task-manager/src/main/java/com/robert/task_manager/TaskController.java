d8c4b78eaaa3cd133c895af966c283883c78889a
Robert <mitricarobert10@gmail.com>
2026-02-19 15:09:16 +0200

    Finish basic CRUD with validations and custom exceptions

--- task-manager/src/main/java/com/robert/task_manager/TaskController.java
+++ task-manager/src/main/java/com/robert/task_manager/TaskController.java
@@ -1,6 +1,11 @@
 package com.robert.task_manager;
 
 
+import com.robert.task_manager.service.TaskService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -9,21 +14,33 @@ import java.util.List;
 @RequestMapping("/api/tasks")
 
 public class TaskController {
-
-    private final TaskRepository taskRepository;
-
-    public TaskController(TaskRepository taskRepository) {
-        this.taskRepository = taskRepository;
-    }
+    @Autowired
+    private TaskService taskService;
 
     @GetMapping
     public List<Task> getAllTask() {
-        return taskRepository.findAll();
+        return taskService.getAllTask();
     }
 
     @PostMapping
-    public Task createTask(@RequestBody Task task) {
-        return taskRepository.save(task);
+    public ResponseEntity<Task> createTask(@Valid @RequestBody Task task) {
+        Task savedTask = taskService.saveTask(task);
+        return new ResponseEntity<>(savedTask, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTask(@PathVariable Long id) {
+        taskService.deleteTask(id);
+    }
+
+    @GetMapping("/{id}")
+    public Task getTaskById(@PathVariable Long id) {
+        return taskService.getTaskById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Task updateTask(@PathVariable Long id, @RequestBody Task taskDetails){
+        return taskService.updateTask(id, taskDetails);
     }
 
 }
---
+++ task-manager/src/main/java/com/robert/task_manager/service/TaskService.java
@@ -0,0 +1,47 @@
+package com.robert.task_manager.service;
+
+import com.robert.task_manager.Task;
+import com.robert.task_manager.TaskRepository;
+import com.robert.task_manager.exception.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TaskService {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public List<Task> getAllTask(){
+        return taskRepository.findAll();
+    }
+
+    public Task saveTask(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public Task getTaskById(Long id) {
+        return taskRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Task ID " + id + " was not found!"));
+    }
+
+    public Task updateTask(Long id, Task taskDetails) {
+        Task task = taskRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Update not possible! Task ID " + id + " not found."));
+
+        task.setTitle(taskDetails.getTitle());
+        task.setDescription(taskDetails.getDescription());
+        task.setCompleted(taskDetails.isCompleted());
+        return taskRepository.save(task);
+    }
+
+    public void deleteTask(Long id) {
+        if (!taskRepository.existsById(id)) {
+            throw new ResourceNotFoundException("Nu pot șterge! Task-ul cu ID " + id + " nu a fost găsit.");
+        }
+        taskRepository.deleteById(id);
+    }
+
+}
